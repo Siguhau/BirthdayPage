1cb8648632a81e6b20f8ba32698cdf69ee0a5c4f
Sigurd Hauan <sigurd.hauan@mazemap.com>
2026-07-03 19:31:14 +0200

    Add ARM64 image publishing workflow

--- BirthdayPage/eslint.config.js
+++ BirthdayPage/eslint.config.js
@@ -22,6 +22,10 @@ export default tseslint.config([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir,
+      },
     },
   },
 ])
--- BirthdayPage/src/utils/utils.tsx
+++ BirthdayPage/src/utils/utils.tsx
@@ -2,7 +2,7 @@
 
 export const getNextValidBirthday = (birthday: Date): Date => {
     const now = new Date();
-    let nextBirthday = new Date(now.getFullYear(), birthday.getMonth(), birthday.getDate());
+    const nextBirthday = new Date(now.getFullYear(), birthday.getMonth(), birthday.getDate());
 
     while (nextBirthday < now) {
         nextBirthday.setFullYear(nextBirthday.getFullYear() + 1);
